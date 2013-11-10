ccc3634a307133bae7f9ab58e3e747b48b22b64e
cavinsb <benjamin.cavins@colorado.edu>
2013-11-09 23:00:12 -0700

    Added lines for all routers

--- start_routers.sh
+++ start_routers.sh
@@ -2,3 +2,7 @@
 
 ./routed_LS A A-log.txt initialization.txt &
 ./routed_LS B B-log.txt initialization.txt &
+./routed_LS C C-log.txt initialization.txt &
+./routed_LS D D-log.txt initialization.txt &
+./routed_LS E E-log.txt initialization.txt &
+./routed_LS F F-log.txt initialization.txt &
