c713201835a67d95660059b94b9de922b8dd5183
cavinsb <benjamin.cavins@colorado.edu>
2013-11-11 21:39:31 -0700

    Added startup message and delay for each router

--- start_routers.sh
+++ start_routers.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+echo starting A...
 ./routed_LS A A-log.txt initialization.txt &
+sleep 1
+echo starting B...
 ./routed_LS B B-log.txt initialization.txt &
+sleep 1
+echo starting C...
 ./routed_LS C C-log.txt initialization.txt &
+sleep 1
+echo starting D...
 ./routed_LS D D-log.txt initialization.txt &
+sleep 1
+echo starting E...
 ./routed_LS E E-log.txt initialization.txt &
+sleep 1
+echo starting F...
 ./routed_LS F F-log.txt initialization.txt &
